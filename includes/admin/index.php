86afa9b28f56bf9997c3f34d65c846e0ecd7450e
rybunial <rybunial@gmail.com>
2021-05-12 23:57:39 +0200

    Dodanie katalogu admin

---
+++ includes/admin/index.php
@@ -0,0 +1,13 @@
+<?php   
+session_start();
+include_once('../includes/naglowek.php');
+include_once('../includes/stopka.php');
+?>
+<html>
+<TITLE> WMatera - Administracja </TITLE>
+<head>
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+        <script type="text/javascript" src="http://jquery.bassistance.de/validate/jquery.validate.js"></script>
+			</head>
+<body>
